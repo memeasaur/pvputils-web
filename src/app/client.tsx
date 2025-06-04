b779e69c9a29de19c29132dd9ea58d45db5db07e
cameron <oniunderscore@gmail.com>
2025-06-04 18:21:12 -0400

    a

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -2,20 +2,28 @@
 import JSZip from "jszip";
 
 export function PackInput() {
-    async function handle(file: File) {
-        const jszip = new JSZip();
-        const zip = await jszip.loadAsync(file);
-        for (const filename of Object.keys(zip.files)) {
-            const content = await zip.files[filename].async("string");
-            console.log(filename, content);
+    async function handle(pack: File | undefined) {
+        if (!pack)
+            alert("invalid pack")
+        else {
+            const updatedResources = await Promise.all(Object.entries((await new JSZip().loadAsync(pack)).files).map(async ([filename, file]) => {
+                // TODO -> option for maintaining backwards compatibility -> adding the old files too
+                // TODO -> progress bar
+                // TODO -> progress console
+                if (filename === "pack.mcmeta") {
+                    // TODO
+                }
+                filename = filename.replace("/blocks/", "/block/")
+                filename = filename.replace("/items/", "/item/")
+                const isDir = file.dir
+                const content = isDir
+                    ? null
+                    : await file.async("uint8array")
+                return [filename, content, isDir]
+            }))
         }
     }
     return (
-        <input type="file" className={"nextButton"} onChange={e => {
-            const pack = e.target.files?.[0]
-            if (pack) {
-                handle(pack)
-            }
-        }}/>
+        <input type="file" className={"nextButton"} onChange={e => handle(e.target.files?.[0])}/>
     )
 }
