b0ba820c0071831cdaf7bbe5f558b73933fd159d
cameron <oniunderscore@gmail.com>
2025-06-03 21:48:21 -0400

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,46 +18,40 @@ export default async function Home() {
         <div
             className="grid items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="grid grid-rows-[30%_1fr] gap-[16px] items-center sm:items-start">
-                {/*<Image*/}
-                {/*  className="dark:invert"*/}
-                {/*  src="/next.svg"*/}
-                {/*  alt="Next.js logo"*/}
-                {/*  width={180}*/}
-                {/*  height={38}*/}
-                {/*  priority*/}
-                {/*/>*/}
                 <Accordion type="single" collapsible defaultValue={"item-1"} className={"row-start-2"}>
+                    <AccordionItem value="item-2">
+                        <AccordionTrigger></AccordionTrigger>
+                        <AccordionContent></AccordionContent>
+                    </AccordionItem>
                     <AccordionItem value="item-1">
-                        <AccordionTrigger>
-                            <div className="flex gap-4 self-end justify-center">
-                                <p className="text-lg self-center">
-                                    fabric-pvputils
-                                </p> {/*TODO idk why tf next has header tags all the same size*/}
-                                <p className={"self-center font-[family-name:var(--font-geist-mono)]"}>
-                                    <UpdateData data={data[0]}></UpdateData>
-                                </p>
-                                <a
-                                    className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-                                    href="https://modrinth.com"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    {/*<Image*/}
-                                    {/*  className="dark:invert"*/}
-                                    {/*  src=""*/}
-                                    {/*  alt="Modrinth logomark"*/}
-                                    {/*  width={20}*/}
-                                    {/*  height={20}*/}
-                                    {/*/>TODO modrinth logo*/}
-                                    modrinth
-                                </a>
-                            </div>
+                        <AccordionTrigger className="flex gap-4 items-center justify-center w-full"> {/*TODO -> make this piece of shit always w-full good fucking luck*/}
+                            <p className="text-lg">
+                                fabric-pvputils
+                            </p> {/*TODO idk why tf next has header tags all the same size*/}
+                            <p className={"font-[family-name:var(--font-geist-mono)]"}>
+                                <UpdateData data={data[0]}></UpdateData>
+                            </p>
+                            <a
+                                className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+                                href="https://modrinth.com"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                {/*<Image*/}
+                                {/*  className="dark:invert"*/}
+                                {/*  src=""*/}
+                                {/*  alt="Modrinth logomark"*/}
+                                {/*  width={20}*/}
+                                {/*  height={20}*/}
+                                {/*/>TODO modrinth logo*/}
+                                modrinth
+                            </a>
                         </AccordionTrigger>
                         {data.map((item, index) => (
-                            <AccordionContent key={item.version}>
-                                <div className="flex gap-4 self-end">
+                            <AccordionContent key={item.version} className={"flex flex-col gap-4"}>
+                                <div className="flex gap-4">
                                     <p className={"font-[family-name:var(--font-geist-mono)]"}>
-                                        v{item.version} - {item.summary}
+                                        {new Date(item.created_at).toLocaleDateString()} v{item.version} - {item.summary}
                                     </p>
                                     <a
                                         href="https://github.com/pvputils/fabricpvputils-oss"
@@ -72,16 +66,13 @@ export default async function Home() {
                                         </>
                                     )}
                                 </div>
-                                <p className={"font-[family-name:var(--font-geist-mono)]"}>
-                                    {new Date(item.created_at).toLocaleDateString()}
-                                </p>
                                 <ol className="font-[family-name:var(--font-geist-mono)]">{/*TODO -> examples for each in accordions*/}
                                     {item.generic_patchnotes.map((item) => (
                                         <li key={item}>{item}</li>
                                     ))}
                                 </ol>
-                                <div className={"flex w-full gap-4"}>{/*TODO -> idk why this needs w-full*/}
-                                    <div className={"w-2/4"}>
+                                <div className={"flex w-full gap-2"}>{/*TODO -> idk why this needs w-full*/}
+                                    <div className={"w-2/4 flex flex-col gap-2"}>
                                         <p>1.9 combat</p>
                                         <ol start={item.generic_patchnotes.length + 1}
                                             className={"font-[family-name:var(--font-geist-mono)]"}>
@@ -90,7 +81,7 @@ export default async function Home() {
                                             ))}
                                         </ol>
                                     </div>
-                                    <div className={"w-2/4"}>
+                                    <div className={"w-2/4 flex flex-col gap-2"}>
                                         <p>1.8 combat</p>
                                         <ol start={item.generic_patchnotes.length + 1}
                                             className={"font-[family-name:var(--font-geist-mono)]"}>
