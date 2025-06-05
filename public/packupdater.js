33a433d8a9d5904b533e216c5d9fe84ffddfbff0
cameron <oniunderscore@gmail.com>
2025-06-05 14:27:24 -0400

    a

--- public/packupdater.js
+++ public/packupdater.js
@@ -1,31 +1,29 @@
 import JSZip from 'jszip';
 
-/**
- * @param {MessageEvent<File>} e
- */
+/** @param {MessageEvent<File>} e */
 self.onmessage = async e => {
-    const updatedResources = Promise.all(Object.entries((await new JSZip().loadAsync(e.data)).files).map(async ([filename, file]) => {
-        // TODO -> option for maintaining backwards compatibility -> adding the old files too
-        // TODO -> progress bar
-        // TODO -> progress console
-        return {
-            filename: filename
+    const updatedPack = new JSZip();
+    self.Promise.all(Object.entries((await new JSZip().loadAsync(e.data)).files)
+        .map(async ([filename, file]) => {
+            // TODO -> option for maintaining backwards compatibility -> adding the old files too
+            // TODO -> progress bar
+            // TODO -> progress console
+            filename = filename
                 .replace("/blocks/", "/block/")
-                .replace("/items/", "/item/"),
-            content: file.dir
+                .replace("/items/", "/item/")
+            const content = file.dir
                 ? null
                 : filename === "pack.mcmeta"
-                    ? (await file.async("string")) // .replace() TODO
+                    ? (await file.async("string"))
+                        // .replace() TODO
                         .replace('"pack_format": 1', '"pack_format": 46')
                     : await file.async("uint8array")
-        }; // TODO -> just use null content as isFile
-    }))
-    const updatedPack = new JSZip()
-    for (const {filename, content} of updatedResources) {
-        if (content === null)
-            updatedPack.folder(filename)
-        else
-            updatedPack.file(filename, content)
-    }
-    // return updatedPack
+
+            // TODO PRETTY SURE THIS IS SAFELY MUTATING UPDATED PACK BECAUSE JAVASCRIPT SINGLE THREADED EVENT LOOP!
+            if (content === null)
+                updatedPack.folder(filename);
+            else
+                updatedPack.file(filename, content);
+        }))
+        .then(() => self.postMessage(updatedPack))
 }
