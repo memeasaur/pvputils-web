69612109382a64814da193bea35c55059bf3383c
cameron <cuckmeron@gmail.com>
2025-08-12 17:23:55 -0400

    a

--- src/app/[connection]/page.tsx
+++ src/app/[connection]/page.tsx
@@ -7,7 +7,7 @@ import {createClient} from "@supabase/supabase-js";
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
-export const revalidate = 604800 // supabase inactivity timeout
+export const revalidate = 302400 // supabase inactivity timeout
 export default async function Page({ params }: { params: {connection: string} }) {
     const supabaseClient = createClient<Database>('https://xapkbnegosbyhmondqti.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhhcGtibmVnb3NieWhtb25kcXRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg3OTgxNTMsImV4cCI6MjA2NDM3NDE1M30.qevIYqIPh3BhiGHj_gppbggv-42RQedaF8Zd-aI5fZA')
     const {
