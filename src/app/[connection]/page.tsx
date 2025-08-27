09841784f2d94d7edf8355c2f5b5f657961fbee9
cameron <cuckmeron@gmail.com>
2025-08-27 13:25:10 -0400

    a

--- src/app/[connection]/page.tsx
+++ src/app/[connection]/page.tsx
@@ -36,111 +36,118 @@ export default async function Page({ params }: { params: {connection: string} })
 
     const connection = (await params).connection // ?
     return (
-        <main
-            style={{
-                backgroundImage: Math.random() < .5
-                    ? "url('https://xapkbnegosbyhmondqti.supabase.co/storage/v1/object/public/packs/fire_layer_0.png')"
-                    : "url('https://xapkbnegosbyhmondqti.supabase.co/storage/v1/object/public/packs/fire_layer_1.png')",
-                backgroundSize: "100% 3200%", // TODO -> fade in as it loads?
-                // height: "100vh",
-                // backgroundPosition: "center bottom",
-                backgroundAttachment: "fixed",
-                imageRendering: "pixelated",
-                animation: "play 3.2s steps(32) infinite"}}
-            className="grid grid-rows-[20vh_1fr_auto] p-8 sm:p-20 place-items-center w-full min-h-screen">{/*TODO -> actually center this*/}
-            <Accordion type="single" collapsible
-                       className={"w-full row-start-2"}
-                       style={{
-                           backdropFilter: "blur(1vw)",
-                           backgroundColor: "rgba(255, 255, 255, 0.95)",
-                           borderRadius: "1vw",
-                           padding: "1vw"
-                       }}
-                       defaultValue={connection === "packupdater" ? "item-2" : connection === "fabric" ? "item-1" : ""}>
-                <AccordionItem value="item-2">
-                    <ChangelogAccordionTrigger
-                        title={"pack-updater"}
-                        version={packUpdaterData[0].version}
-                        summary={packUpdaterData[0].summary}
-                        updateData={
-                            <>
-                                accepts (1.7.10).zip, returns (1.21.4).zip
-                            </>}
-                        modrinthButton={null}
-                    />
-                    <AccordionContent className={"flex flex-col gap-4"}>
-                        <PackUpdater></PackUpdater>{/*TODO -> allow non-compressed files (?)*/}
-                        <OverviewAccordionContent
-                            created_at={packUpdaterData[0].created_at}
-                            generic_patchnotes={packUpdaterData[0].generic_patchnotes}
-                            extra_patchnotes={null}
+        <div style={{
+            backgroundImage: "url('https://xapkbnegosbyhmondqti.supabase.co/storage/v1/object/public/packs/vignette.png')",
+            backgroundSize: 'cover',
+            mixBlendMode: 'multiply'
+        }}>
+            <main
+                style={{
+                    backgroundImage: Math.random() < .5
+                        ? "url('https://xapkbnegosbyhmondqti.supabase.co/storage/v1/object/public/packs/fire_layer_0.png')"
+                        : "url('https://xapkbnegosbyhmondqti.supabase.co/storage/v1/object/public/packs/fire_layer_1.png')",
+                    backgroundSize: "100% 3200%", // TODO -> fade in as it loads?
+                    // height: "100vh",
+                    // backgroundPosition: "center bottom",
+                    backgroundAttachment: "fixed",
+                    imageRendering: "pixelated",
+                    animation: "play 3.2s steps(32) infinite"
+                    }}
+                className="grid grid-rows-[20vh_1fr_auto] p-8 sm:p-20 place-items-center w-full min-h-screen">{/*TODO -> actually center this*/}
+                <Accordion type="single" collapsible
+                           className={"w-full row-start-2"}
+                           style={{
+                               backdropFilter: "blur(1vw)",
+                               backgroundColor: "rgba(255, 255, 255, 0.95)",
+                               borderRadius: "1vw",
+                               padding: "1vw"
+                           }}
+                           defaultValue={connection === "packupdater" ? "item-2" : connection === "fabric" ? "item-1" : ""}>
+                    <AccordionItem value="item-2">
+                        <ChangelogAccordionTrigger
+                            title={"pack-updater"}
+                            version={packUpdaterData[0].version}
+                            summary={packUpdaterData[0].summary}
+                            updateData={
+                                <>
+                                    accepts (1.7.10).zip, returns (1.21.4).zip
+                                </>}
+                            modrinthButton={null}
                         />
-                    </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-1" className={"w-full"}>
-                    <ChangelogAccordionTrigger
-                        title={"fabric-pvputils"}
-                        version={pvpUtilsData0.version}
-                        summary={pvpUtilsData0.summary}
-                        updateData={<>
-                            ({pvpUtilsData0.minecraft_versions}) {pvpUtilsData0.nullable_dependencies && (
-                            <>
-                                depends: {pvpUtilsData0.nullable_dependencies.map((item, index) => (
-                                <React.Fragment key={index}>
-                                    {index > 0 && ', '}
-                                    {item}
-                                </React.Fragment>
-                            ))}
-                            </>
-                        )}
-                        </>}
-                        modrinthButton={<a
-                            className="nextButton"
-                            href="https://modrinth.com/mod/fabric-pvp-utils"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            {/*<Image*/}
-                            {/*  className="dark:invert"*/}
-                            {/*  src=""*/}
-                            {/*  alt="Modrinth logomark"*/}
-                            {/*  width={20}*/}
-                            {/*  height={20}*/}
-                            {/*/>TODO modrinth logo*/}
-                            modrinth
-                        </a>}
-                    />
-                    <AccordionContent className={"flex flex-col gap-4"}>
-                        <a
-                            href="https://github.com/pvputils/fabricpvputils-oss"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <Badge variant="secondary">github</Badge>
-                        </a>
-                        <ReactMarkdown remarkPlugins={[remarkGfm]}>
-                            {pvpUtilsData0.markdown}
-                        </ReactMarkdown>
-                    </AccordionContent>
-                </AccordionItem>
-                {/*<AccordionItem value="item-3">*/}
-                {/*    <AccordionTrigger>*/}
-                {/*        <s>fabric-pvputils+</s>*/}
-                {/*    </AccordionTrigger>*/}
-                {/*    <AccordionContent>*/}
-                {/*        todo*/}
-                {/*    </AccordionContent>*/}
-                {/*</AccordionItem>TODO */}
-            </Accordion>
-            <a // TODO -> make this piece of fucking shit hug the top GL
-                href="https://github.com/memeasaur/pvputils-web"
-                target="_blank"
-                rel="noopener noreferrer"
-                className={"row-start-3 justify-self-end"}
-            >
-                <Badge variant="secondary">github</Badge>
-            </a>
-        </main>
+                        <AccordionContent className={"flex flex-col gap-4"}>
+                            <PackUpdater></PackUpdater>{/*TODO -> allow non-compressed files (?)*/}
+                            <OverviewAccordionContent
+                                created_at={packUpdaterData[0].created_at}
+                                generic_patchnotes={packUpdaterData[0].generic_patchnotes}
+                                extra_patchnotes={null}
+                            />
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="item-1" className={"w-full"}>
+                        <ChangelogAccordionTrigger
+                            title={"fabric-pvputils"}
+                            version={pvpUtilsData0.version}
+                            summary={pvpUtilsData0.summary}
+                            updateData={<>
+                                ({pvpUtilsData0.minecraft_versions}) {pvpUtilsData0.nullable_dependencies && (
+                                <>
+                                    depends: {pvpUtilsData0.nullable_dependencies.map((item, index) => (
+                                    <React.Fragment key={index}>
+                                        {index > 0 && ', '}
+                                        {item}
+                                    </React.Fragment>
+                                ))}
+                                </>
+                            )}
+                            </>}
+                            modrinthButton={<a
+                                className="nextButton"
+                                href="https://modrinth.com/mod/fabric-pvp-utils"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                {/*<Image*/}
+                                {/*  className="dark:invert"*/}
+                                {/*  src=""*/}
+                                {/*  alt="Modrinth logomark"*/}
+                                {/*  width={20}*/}
+                                {/*  height={20}*/}
+                                {/*/>TODO modrinth logo*/}
+                                modrinth
+                            </a>}
+                        />
+                        <AccordionContent className={"flex flex-col gap-4"}>
+                            <a
+                                href="https://github.com/pvputils/fabricpvputils-oss"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <Badge variant="secondary">github</Badge>
+                            </a>
+                            <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                                {pvpUtilsData0.markdown}
+                            </ReactMarkdown>
+                        </AccordionContent>
+                    </AccordionItem>
+                    {/*<AccordionItem value="item-3">*/}
+                    {/*    <AccordionTrigger>*/}
+                    {/*        <s>fabric-pvputils+</s>*/}
+                    {/*    </AccordionTrigger>*/}
+                    {/*    <AccordionContent>*/}
+                    {/*        todo*/}
+                    {/*    </AccordionContent>*/}
+                    {/*</AccordionItem>TODO */}
+                </Accordion>
+                <a // TODO -> make this piece of fucking shit hug the top GL
+                    href="https://github.com/memeasaur/pvputils-web"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={"row-start-3 justify-self-end"}
+                >
+                    <Badge variant="secondary">github</Badge>
+                </a>
+            </main>
+        </div>
     );
 }
 
